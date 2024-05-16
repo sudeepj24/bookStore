366e8f9d90bdda0e42b1fcb3164f827c5352e964
ㅤSudeep <spj24india@gmail.com>
2024-05-16 22:30:04 +0530

    first commit

---
+++ src/main/java/com/bookStore/entity/Book.java
@@ -0,0 +1,57 @@
+package com.bookStore.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String author;
+    private String price;
+
+    // Default constructor
+    public Book() {
+        super();
+        // You can initialize default values or leave them empty
+    }
+
+    // Getters and setters for id, name, author, and price go here
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
